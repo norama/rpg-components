a01a7e3192864e6edb18ca2dfd8545522fa01475
Nora Mate <norama.matema@gmail.com>
2023-07-10 00:33:37 +0200

    MF

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import cssInject from 'vite-plugin-css-injected-by-js'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [solid(), tsconfigPaths()],
+  plugins: [solid(), tsconfigPaths(), cssInject()],
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      input: resolve(__dirname, 'src/components/rpg-name/NameInput.tsx'),
+      preserveEntrySignatures: 'exports-only',
+      external: ['solid-js', '@modular-forms/solid'],
+      output: {
+        entryFileNames: 'bundle.js',
+        format: 'esm',
+      },
+    },
+  },
 })
